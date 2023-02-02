9deda59495c6a11366ece895106250976ce95238
alex-rex4 <hello4alex@gmail.com>
2023-02-02 23:50:05 +0300

    inputs changes colours and inscriptions appears

--- main.js
+++ main.js
@@ -32,43 +32,62 @@ function validateIndex(){
         }
 }
 
+function generateInscription(e, q){
+
+    console.log("sfsfsdfsf")
+    if(document.getElementById(e).value!==""){
+        document.getElementById(q).classList.add('show')
+    }else{
+        document.getElementById(q).classList.remove('show')
+    }
+}
 
 function submit(){
     flag = true;
     if(((document.getElementById('name').value)=="")) {
         document.getElementById('recipient-data-name-error').classList.add('recipient-data-error-show');
+        document.getElementById('name').classList.add('error-input-text');
         flag = false;
     }else{
         document.getElementById('recipient-data-name-error').classList.remove('recipient-data-error-show')
+        document.getElementById('name').classList.remove('error-input-text');
     }
 
     if(((document.getElementById('surname').value)=="")) {
         document.getElementById('recipient-data-surname-error').classList.add('recipient-data-error-show');
+        document.getElementById('surname').classList.add('error-input-text');
         flag = false;
     }else{
-        document.getElementById('recipient-data-surname-error').classList.remove('recipient-data-error-show')
+        document.getElementById('recipient-data-surname-error').classList.remove('recipient-data-error-show');
+        document.getElementById('surname').classList.remove('error-input-text');
     }
 
 
     if((!emailReg.test(document.getElementById('email').value))||((document.getElementById('email').value)=="")) {
         document.getElementById('recipient-data-email-error').classList.add('recipient-data-error-show');
+        document.getElementById('email').classList.add('error-input-text');
         flag = false;
     }else{
-        document.getElementById('recipient-data-email-error').classList.remove('recipient-data-error-show')
+        document.getElementById('recipient-data-email-error').classList.remove('recipient-data-error-show');
+        document.getElementById('email').classList.remove('error-input-text');
     }
 
     if((!phoneReg.test(document.getElementById('phone').value))||((document.getElementById('phone').value)=="")) {
         document.getElementById('recipient-data-phone-error').classList.add('recipient-data-error-show');
+        document.getElementById('phone').classList.add('error-input-text');
         flag = false;
     }else{
-        document.getElementById('recipient-data-phone-error').classList.remove('recipient-data-error-show')
+        document.getElementById('recipient-data-phone-error').classList.remove('recipient-data-error-show');
+        document.getElementById('phone').classList.remove('error-input-text');
     }
 
     if((!indexReg.test(document.getElementById('index').value))||((document.getElementById('index').value)=="")) {
         document.getElementById('recipient-data-index-error').classList.add('recipient-data-error-show');
+        document.getElementById('index').classList.add('error-input-text');
         flag = false;
     }else{
-        document.getElementById('recipient-data-index-error').classList.remove('recipient-data-error-show')
+        document.getElementById('recipient-data-index-error').classList.remove('recipient-data-error-show');
+        document.getElementById('index').classList.remove('error-input-text');
     }
 
     if (flag == true){
@@ -135,4 +154,13 @@ function closeDialog(id){
 
 function openDialog(id){
     document.getElementById(id).classList.add('dialog-show');
+}
+
+function generateHeader (id, headerId) {
+    if (document.getElementById(id).value=="") {
+        document.getElementById(headerId).classList.remove('show');
+    } else {
+        document.getElementById(headerId).classList.add('show');
+    }
+   
 }
